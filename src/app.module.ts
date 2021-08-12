f43c8f6b5048a6fef1dcc5b1a81ccf5e2b6e88c3
tonivj5 <t.villena@lionline.de>
2021-08-12 18:47:46 +0200

    chore: doesn't work any token

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,10 @@
-import { Inject, Injectable, Module, Optional } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  Module,
+  OnModuleInit,
+  Optional,
+} from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,25 +12,42 @@ import { AppService } from './app.service';
 @Injectable()
 class SpecialService {
   constructor(public readonly service: AppService) {
-    console.log(`Hey! SpecialService here, my constructor has been called and the AppService is inyected correctly: `, service);
+    console.log(
+      `Hey! SpecialService here, my constructor has been called and the AppService is inyected correctly: `,
+      service,
+    );
   }
 }
 
 const SpecialSymbol = Symbol('Special Symbol');
+class ClassOtherToken {}
+const SimpleOtherToken = 'Token';
 
 // If you uncomment any of two comments below (or both), all works as expected
 @Module({
   controllers: [AppController],
-  providers: [AppService, /* SpecialService, */ { provide: SpecialSymbol, useClass: SpecialService }],
+  providers: [
+    AppService,
+    /* SpecialService, */ { provide: SpecialSymbol, useClass: SpecialService },
+    { provide: ClassOtherToken, useClass: SpecialService },
+    { provide: SimpleOtherToken, useClass: SpecialService },
+  ],
 })
-export class AppModule {
+export class AppModule implements OnModuleInit {
   constructor(
-    @Optional() /* @Inject(SpecialSymbol)  */specialService: SpecialService,
+    @Optional() /* @Inject(SpecialSymbol)  */ specialService: SpecialService,
     private moduleRef: ModuleRef,
   ) {
-    // It works as expected...
-    // console.log(`from ModuleRef`, this.moduleRef.get(SpecialService))
-    console.log(`from ModuleRef`, this.moduleRef.get(SpecialSymbol))
-    console.log(`from DI`, specialService)
+    console.log(`from ModuleRef`, this.moduleRef.get(SpecialSymbol));
+    console.log(`from ModuleRef`, this.moduleRef.get(ClassOtherToken));
+    console.log(`from ModuleRef`, this.moduleRef.get(SimpleOtherToken));
+    console.log(`from DI`, specialService);
+  }
+
+  onModuleInit() {
+    console.log(
+      `from ModuleRef on ModuleInit`,
+      this.moduleRef.get(SpecialSymbol),
+    );
   }
 }
